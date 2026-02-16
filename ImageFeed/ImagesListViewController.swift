1e96126d39749192d807576184d9c86372597aba
miracle768 <L0bastoff.ilya@yandex.ru>
2026-02-16 23:31:53 +0300

    Добавлен экран загрузки, uitableView, а также delegate and dataSource

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
