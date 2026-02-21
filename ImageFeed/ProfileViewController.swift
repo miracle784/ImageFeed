c0f128407624c1dec8b4f1b8f753f9c5754ebca7
miracle768 <L0bastoff.ilya@yandex.ru>
2026-02-22 00:21:21 +0300

    Добавлен экран отдельного изображения SingleImageViewController, На экране SingleImageViewController реализована кнопка «Поделиться»

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
